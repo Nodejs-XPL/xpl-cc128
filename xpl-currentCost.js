6551151bc3c328cf8ce7e44d3a7fb4a4b39f5838
oeuillot <olivier.oeuillot@gmail.com>
2015-11-09 14:16:07 +0100

    fix heapdump

--- xpl-currentCost.js
+++ xpl-currentCost.js
@@ -16,126 +16,135 @@ Xpl.fillCommander(commander);
 commander.option("--heapDump", "Enable heap dump (require heapdump)");
 
 commander.command('listSerialPort').description("List serial ports").action(
-		function() {
+    function() {
 
-			console.log("List serial ports:");
-			serialport.list(function(err, ports) {
-				if (err) {
-					console.log("List performs error : " + err);
-					process.exit(0);
-					return;
-				}
+      console.log("List serial ports:");
+      serialport.list(function(err, ports) {
+        if (err) {
+          console.log("List performs error : " + err);
+          process.exit(0);
+          return;
+        }
 
-				ports.forEach(function(port) {
-					console.log("  Port name='" + port.comName + "' pnpId='" + port.pnpId + "' manufacturer='" +
-							port.manufacturer + "'");
-				});
-				console.log("End of list");
+        ports.forEach(function(port) {
+          console.log("  Port name='" + port.comName + "' pnpId='" +
+              port.pnpId + "' manufacturer='" + port.manufacturer + "'");
+        });
+        console.log("End of list");
 
-			});
-		});
+      });
+    });
 
-commander.command('start').description("Start processing CC128 datas").action(function() {
-	console.log("Start");
+commander
+    .command('start')
+    .description("Start processing CC128 datas")
+    .action(
+        function() {
+          console.log("Start");
 
-	if (!commander.serialPort) {
-		switch (os.platform()) {
-		case "win32":
-			commander.serialPort = "COM4";
-			break;
-		case "linux":
-			commander.serialPort = "/dev/serial/by-id/usb-Prolific_Technology_Inc._USB-Serial_Controller-if00-port0";
-			break;
-		}
+          if (!commander.serialPort) {
+            switch (os.platform()) {
+            case "win32":
+              commander.serialPort = "COM4";
+              break;
+            case "linux":
+              commander.serialPort = "/dev/serial/by-id/usb-Prolific_Technology_Inc._USB-Serial_Controller-if00-port0";
+              break;
+            }
 
-		console.log("Use default serial port : " + commander.serialPort);
-	}
+            console.log("Use default serial port : " + commander.serialPort);
+          }
 
-	var sp = new serialport.SerialPort(commander.serialPort, {
-		baudrate: 57600,
-		databits: 8,
-		stopbits: 1,
-		parity: 'none',
-		rtscts: false,
-		parser: serialport.parsers.readline("\n")
-	});
+          commander.deviceAliases = Xpl
+              .loadDeviceAliases(commander.deviceAliases);
 
-	sp.on("open", function(error) {
-		try {
-			if (error) {
-				console.log("Can not open serial device '" + commander.serialPort + "'", error);
-				process.exit(1);
-				return;
-			}
-			console.log("Serial device '" + commander.serialPort + "' opened.");
+          var sp = new serialport.SerialPort(commander.serialPort, {
+            baudrate : 57600,
+            databits : 8,
+            stopbits : 1,
+            parity : 'none',
+            rtscts : false,
+            parser : serialport.parsers.readline("\n")
+          });
 
-			if (!commander.xplSource) {
-				var hostName = os.hostname();
-				if (hostName.indexOf('.') > 0) {
-					hostName = hostName.substring(0, hostName.indexOf('.'));
-				}
+          sp.on("open", function(error) {
+            try {
+              if (error) {
+                console.log("Can not open serial device '" +
+                    commander.serialPort + "'", error);
+                process.exit(1);
+                return;
+              }
+              console.log("Serial device '" + commander.serialPort +
+                  "' opened.");
 
-				commander.xplSource = "cc128." + hostName;
-			}
+              if (!commander.xplSource) {
+                var hostName = os.hostname();
+                if (hostName.indexOf('.') > 0) {
+                  hostName = hostName.substring(0, hostName.indexOf('.'));
+                }
 
-			var xpl = new Xpl(commander);
+                commander.xplSource = "cc128." + hostName;
+              }
 
-			xpl.on("error", function(error) {
-				console.log("XPL error", error);
-			});
+              var xpl = new Xpl(commander);
 
-			xpl.bind(function(error) {
-				if (error) {
-					console.log("Can not open xpl bridge ", error);
-					process.exit(2);
-					return;
-				}
+              xpl.on("error", function(error) {
+                console.log("XPL error", error);
+              });
 
-				console.log("Xpl bind succeed ");
+              xpl.bind(function(error) {
+                if (error) {
+                  console.log("Can not open xpl bridge ", error);
+                  process.exit(2);
+                  return;
+                }
 
-				new CC128Serial(function(data, callback) {
-					// console.log("Write '" + data + "'");
-					sp.write(data, callback);
+                console.log("Xpl bind succeed ");
 
-				}, function(body, callback) {
-					xpl.sendXplTrig(body, callback);
+                new CC128Serial(function(data, callback) {
+                  // console.log("Write '" + data + "'");
+                  sp.write(data, callback);
 
-				}, commander, function(error, cc128) {
-					if (error) {
-						console.log("Can not initialize CC128 engine ", error);
-						process.exit(3);
-						return;
-					}
+                }, function(body, callback) {
+                  xpl.sendXplTrig(body, callback);
 
-					sp.on('data', function(data) {
-						// console.log('data received: ' + data+"'");
+                }, commander, function(error, cc128) {
+                  if (error) {
+                    console.log("Can not initialize CC128 engine ", error);
+                    process.exit(3);
+                    return;
+                  }
 
-						cc128.processSerialData(data);
-					});
+                  sp.on('data', function(data) {
+                    // console.log('data received: ' + data+"'");
 
-					sp.on('close', function() {
-						console.log('close received: ' + data);
+                    cc128.processSerialData(data);
+                  });
 
-						cc128.close();
+                  sp.on('close', function() {
+                    console.log('close received: ' + data);
 
-						xpl.close();
-					});
+                    cc128.close();
 
-					xpl.on("xpl:xpl-cmnd", function(message) {
-						cc128.processXplMessage(message);
-					});
-				});
+                    xpl.close();
+                  });
 
-			});
-		} catch (x) {
-			console.log(x);
-		}
-	});
-});
+                  xpl.on("xpl:xpl-cmnd", function(message) {
+                    cc128.processXplMessage(message);
+                  });
+                });
+
+              });
+            } catch (x) {
+              console.log(x);
+            }
+          });
+        });
 
 commander.parse(process.argv);
 
-if (commander.headDump) {
-	var heapdump = require("heapdump");
-	console.log("***** HEAPDUMP enabled **************");
+if (commander.heapDump) {
+  var heapdump = require("heapdump");
+  console.log("***** HEAPDUMP enabled **************");
 }
